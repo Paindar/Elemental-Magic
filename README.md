400c5762fefd5ddd3a745152f45838a267d325b8
Paindar <1944798085@qq.com>
2018-07-09 23:11:27 +0800

    apply basic modpack struct
    这是不是意味着我的签到之路又开始了？

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ElementalMagic
+
+## Build
+
+Simply use `gradlew build`. If you encounter `Unknown constant: 18` error, `gradlew build` again and it should be fine.
+
+## Workspace
+
+An IDEA workspace with everything required can be set up using `gradlew idea`. You must have scala plugin installed.
+
+Eclipse workspace is not tested.
+
+Issue Submission
+============
+
+Any issue should be prefixed with the version associated. For example, `[1.0.0] Some unknown bug`.
+
+## Bug
+
+Please provide the following information if you are to submit a bug:
+
+* A brief description of the bug
+* A repeatible way to reproduce the bug, if you can find any
+* Crash report and the latest client/server log
---
+++ src/main/java/me/paindar/ElementalMagic.java
@@ -0,0 +1,28 @@
+package me.paindar;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ElementalMagic.MODID, name = ElementalMagic.NAME, version = ElementalMagic.VERSION)
+public class ElementalMagic
+{
+    public static final String MODID = "elementalmagic";
+    public static final String NAME = "Elemental Magic";
+    public static final String VERSION = "1.0";
+
+    public static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
